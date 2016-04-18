bbb82cf4ac8ed821f5e921815613657a99760675
James Dabbs <jamesdabbs@gmail.com>
2016-04-18 17:00:49 -0400

    Render a whole table row using javascript

--- js/weather/before/app.js
+++ js/weather/before/app.js
@@ -3,5 +3,15 @@
     var s = document.getElementById("day-table-body");
 
     var dayName = "Tuesday";
-    s.innerHTML = `<tr><td>${dayName}</td><td>`;
+    var dayWeather = "H";
+    var dayHigh = 79;
+    var dayLow = 55;
+
+    s.innerHTML = `
+      <tr class="day">
+        <td class="day__name">${dayName}</td>
+        <td class="day__weather"><i data-icon="${dayWeather}"></i></td>
+        <td class="day__high">${dayHigh}</td>
+        <td class="day__low">${dayLow}</td>
+      </tr>`;
 })();
