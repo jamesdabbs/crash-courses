1a6721d62928e5c5fc7e3b5c78a116a3dbdd31e8
James Dabbs <jamesdabbs@gmail.com>
2016-04-18 17:02:07 -0400

    Pull out a function for rendering a day
    as we'll soon want to do this for multiple days

--- js/weather/before/app.js
+++ js/weather/before/app.js
@@ -2,16 +2,16 @@
     console.log("It works")
     var s = document.getElementById("day-table-body");
 
-    var dayName = "Tuesday";
-    var dayWeather = "H";
-    var dayHigh = 79;
-    var dayLow = 55;
+    function buildDayRow(day) {
+        return `
+          <tr class="day">
+            <td class="day__name">${day.name}</td>
+            <td class="day__weather"><i data-icon="${day.weather}"></i></td>
+            <td class="day__high">${day.high}</td>
+            <td class="day__low">${day.low}</td>
+          </tr>`;
+    }
 
-    s.innerHTML = `
-      <tr class="day">
-        <td class="day__name">${dayName}</td>
-        <td class="day__weather"><i data-icon="${dayWeather}"></i></td>
-        <td class="day__high">${dayHigh}</td>
-        <td class="day__low">${dayLow}</td>
-      </tr>`;
+    var tuesday = { name: "Tuesday", weather: "H", high: 79, low: 55 };
+    s.innerHTML = buildDayRow(tuesday);
 })();
